339e364dbf0d22d61aae911ff60e4fc4240db151
PwolGithub <850866596@qq.com>
2018-11-12 20:48:55 +0800

    啦啦啦嗄

---
+++ pwol-background-consumer/src/test/java/cn/pwol/pwolbackgroundconsumer/PwolBackgroundConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.pwol.pwolbackgroundconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PwolBackgroundConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
